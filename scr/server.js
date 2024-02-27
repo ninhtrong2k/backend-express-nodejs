e1fdfc61bd18444cce6561fb5d9c0a66fe18999b
ninhtrong2k <ninhtrongchannel@gmail.com>
2024-02-27 23:31:12 +0700

    video 32

--- scr/server.js
+++ scr/server.js
@@ -3,8 +3,7 @@ const app = express();
 require('dotenv').config();
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
-
+const connection = require('./config/database')
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 configViewEngine(app);
@@ -12,13 +11,6 @@ configViewEngine(app);
 app.use('/test', webRoutes);
 app.use('/', webRoutes);
 
-const connection = mysql.createConnection({
-  host: '192.168.200.177',
-  port: 3306,
-  user: 'root',
-  password: 'toandaik',
-  database: 'backend_nodejs'
-});
 
 connection.query(
   'SELECT * FROM User', 
