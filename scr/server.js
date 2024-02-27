a0755bdf6c9212f2dbdc85ca2d796dd5778da93f
ninhtrong2k <ninhtrongchannel@gmail.com>
2024-02-27 23:23:08 +0700

    video 30

--- scr/server.js
+++ scr/server.js
@@ -3,12 +3,30 @@ const app = express();
 require('dotenv').config();
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const port = process.env.PORT  || 8888;
+const mysql = require('mysql2');
+
+const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 configViewEngine(app);
 
-app.use('/test',webRoutes);
-app.use('/',webRoutes);
+app.use('/test', webRoutes);
+app.use('/', webRoutes);
+
+const connection = mysql.createConnection({
+  host: '192.168.200.177',
+  port: 3306,
+  user: 'root',
+  password: 'toandaik',
+  database: 'backend_nodejs'
+});
+
+connection.query(
+  'SELECT * FROM User', 
+  function(err, results, fields) {
+    console.log(results);
+    console.log(fields);
+  }
+);
 
 app.listen(port, hostname => {
   console.log(`Example app listening on port ${port}`);
