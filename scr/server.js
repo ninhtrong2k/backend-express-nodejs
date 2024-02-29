809fcfd8b4075830ba4a2f95308c468e88ad2afc
ninhtrong2k <ninhtrongchannel@gmail.com>
2024-02-29 20:27:51 +0700

    video 59

--- scr/server.js
+++ scr/server.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const connection = require('./config/database')
+const mongoose = require('mongoose');
+
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 // config req.body
@@ -16,20 +18,26 @@ configViewEngine(app);
 app.use('/test', webRoutes);
 app.use('/', webRoutes);
 
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+const Kitten = mongoose.model('Kitten', kittySchema);
+const silence = new Kitten({ name: 'Hoi Dan it Cat' });
+silence.save();
 // Run db before server
-(async() => {
+(async () => {
   try {
-    await  connection();
+    await connection();
     app.listen(port, hostname => {
       console.log(`Backend Zero listening on port ${port}`);
     })
-  }catch (error){
+  } catch (error) {
     console.log(">>> erro connection to db", error)
   }
 })()
 
 // connection.query(
-//   'SELECT * FROM User', 
+//   'SELECT * FROM User',
 //   function(err, results, fields) {
 //     console.log(results);
 //     console.log(fields);
